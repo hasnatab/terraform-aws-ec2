ac5e871bb0f8a80de12f3bbf314351554abdadf8
hasnatab <hak158@mun.ca>
2023-11-26 15:49:15 -0700

    first commit after creating the EC2 instance on AWS

---
+++ first_instance.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+	region = "us-east-1"
+}
+
+resource "aws_instance" "frist_ec2_instance" {
+	ami = "ami-0230bd60aa48260c6"
+	instance_type = "t2.micro"
+	availability_zone = "us-east-1a"
+	key_name = "terra-key"
+	vpc_security_group_ids = ["sg-048d6ba61a0bdc23d"]
+	tags = {
+		Name = "TerraformInstance"
+	}
+}
